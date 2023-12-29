92fffbdc1ba5d8d1dbf3f1afc5856d35c0d99d77
Nicolas Frasson <nicfra0710@gmail.com>
2023-12-29 12:12:52 -0300

    imp: implement login and add JWT feature

--- src/functions/userLogin.ts
+++ src/functions/userLogin.ts
@@ -1,10 +1,39 @@
+import { sign } from "jsonwebtoken";
+import { User } from "../user.entity";
 import { LambdaDefaultHandler } from "../default.handler";
+import { connection } from "../typerorm.connection";
+import { compare } from "bcryptjs";
 import { IsString, IsEmail } from "class-validator";
 
 export const login = async (data: UserLoginDto) => {
+  const dataSource = await connection();
+  const userRepository = dataSource.getRepository(User);
+
+  const user = await userRepository.findOne({
+    where: { userEmail: data.userEmail },
+  });
+
+  if (!user) {
+    return {
+      statusCode: 404,
+      body: "User not found",
+    };
+  }
+
+  const valid = await compare(data.userPassword, user.userPassword);
+
+  if (!valid) {
+    return {
+      statusCode: 401,
+      body: "Invalid password",
+    };
+  }
+
+  const token = await sign({ userId: user.userId }, process.env.JWT_SECRET);
+
   return {
     statusCode: 200,
-    body: data,
+    body: {token},
   };
 };
 
@@ -16,4 +45,5 @@ export class UserLoginDto {
   userPassword: string;
 }
 
-export const handler = new LambdaDefaultHandler(login, UserLoginDto).handleAPIGatewayEvent;
+export const handler = new LambdaDefaultHandler(login, UserLoginDto)
+  .handleAPIGatewayEvent;
--- src/functions/userRegister.ts
+++ src/functions/userRegister.ts
@@ -1,19 +1,24 @@
-import { LambdaDefaultHandler } from "../default.handler";
-import { IsString, IsEmail } from "class-validator";
+import { sign } from "jsonwebtoken";
 import { User } from "../user.entity";
+import { hash } from "bcryptjs";
+import { IsString, IsEmail } from "class-validator";
 import { connection } from "../typerorm.connection";
+import { LambdaDefaultHandler } from "../default.handler";
 
 export const register = async (data: UserRegisterDto) => {
   const dataSource = await connection();
   const userRepository = dataSource.getRepository(User);
 
   const user = new User(data);
+  user.userPassword = await hash(data.userPassword, 8);
 
   await userRepository.save(user);
 
+  const token = await sign({ userId: user.userId }, process.env.JWT_SECRET);
+
   return {
     statusCode: 201,
-    body: { data, user },
+    body: { token },
   };
 };
 
